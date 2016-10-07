c9fe476d4139e79198002606e526b18907f451d6
Ryo <kanadajinlee@yahoo.ca>
2016-10-07 17:06:02 +0000

    just exported a file to another file

---
+++ gonna-export-module.js
@@ -0,0 +1,20 @@
+module.exports = {
+  message: "This message is from gonna-export-module.js",
+  PHI: 1.618,
+  explain: function() {
+    console.log("(X+Y) is to X as X is to Y: (X+Y)≈" + this.PHI);
+  }
+}
+
+
+//would also work as the following...
+      // var trigger = {
+      //   message: "This message is from gonna-export-module.js",
+      //   PHI: 1.618,
+      //   explain: function() {
+      //     console.log("(X+Y) is to X as X is to Y: (X+Y)≈" + this.PHI);
+      //   }
+      // }
+      // module.exports = trigger
+//but then you'd have to change everything that says "secretMsgAndMore"
+//to "secretMsgAndMore.trigger" in the file that requires this
---
+++ gonna-require-module.js
@@ -0,0 +1,6 @@
+var secretMsgAndMore = require("./gonna-export-module.js");
+
+console.log(secretMsgAndMore.message);
+console.log(secretMsgAndMore.PHI);
+
+secretMsgAndMore.explain();  // invokes the function `explain`
